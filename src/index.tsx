815b096c266a25c365bfc9cf5033a62fdedb6be3
Neal Stewart <nea@trustpilot.com>
2018-12-23 16:04:03 +0100

    Working but slow

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import './index.css';
-import { range } from 'lodash';
+import { range, difference } from 'lodash';
 
 type Vector2d = [number, number];
 
@@ -53,14 +53,24 @@ const update = (canvas: HTMLCanvasElement, state:State, timeDiff: number) => {
     ];
   });
 
+  const shouldStop = (p: Particle) => isAtBottom(p, canvas.height) || state.deadSnow.some(s => overlap(p, s));
+
   state.deadSnow = [
     ...state.deadSnow, 
-    ...state.particles.filter(p => isAtBottom(p, canvas.height))
+    ...state.particles.filter(shouldStop)
   ];
 
-  state.particles = state.particles.filter(p => {
-    return p.location[0] >= 0 && p.location[0] < canvas.width && !isAtBottom(p, canvas.height);
+  state.deadSnow.forEach((p) => {
+    if (!isAtBottom(p, canvas.height)) {
+      p.location[1] = Math.min(canvas.height, p.location[1] + 0.2); 
+    }
   });
+
+  state.deadSnow = state.deadSnow.filter(({radius}) => radius > 0.5);
+  state.particles = difference(state.particles, state.deadSnow);
+
+  state.particles = state.particles.filter(p =>
+    p.location[0] >= 0 && p.location[0] < canvas.width);
 }
 
 function createParticles(touchPoint: Vector2d) {
@@ -79,6 +89,14 @@ function createParticles(touchPoint: Vector2d) {
   }));
 }
 
+const subtractVector = ([a1, a2]: Vector2d, [b1, b2]: Vector2d): Vector2d =>
+  [a1 - b1, a2 - b2];
+
+const lengthVector = ([x, y]: Vector2d) => Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+const overlap = (a: Particle, b: Particle) =>
+  lengthVector(subtractVector(a.location, b.location)) <= (a.radius + b.radius);
+
 (() => {
   let state: State = {
     particles: [],
@@ -90,8 +108,12 @@ function createParticles(touchPoint: Vector2d) {
   canvas.height = window.innerHeight;
 
   const touchHandler = (evt: TouchEvent) => {
-    const allNewParticles = Array.from(evt.touches)
-      .reduce((all, t) => [...all, ...createParticles([t.clientX, t.clientY])], []);
+    const touchArray = Array.from(evt.touches).map<Vector2d>(touch => [touch.clientX, touch.clientY]);
+
+    const allNewParticles = 
+      touchArray.reduce(
+        (all, t) => [...all, ...createParticles(t)],
+        []);
 
     state.particles = [
       ...allNewParticles,
@@ -107,7 +129,6 @@ function createParticles(touchPoint: Vector2d) {
     render(canvas, state);
 
     const currentTime = Date.now();
-
     update(canvas, state, currentTime - lastTime);
 
     lastTime = currentTime;
